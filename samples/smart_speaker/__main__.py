f486593a9824c5fd7fba6ffcc9a48922a116c1d9
michaelperel <michaelsethperel@gmail.com>
2019-01-27 11:09:46 -0800

    cleaned up smart_speaker example (#55)

--- samples/smart_speaker/__main__.py
+++ samples/smart_speaker/__main__.py
@@ -3,30 +3,28 @@ from pathlib import Path
 from putput import generate_utterances_and_tokens, generate_utterance_pattern_and_tokens, CombinationOptions
 
 
-def _custom_token_handler(token: str, tokenized_phrase: str) -> str:
-    return "[{token}({tokenized_phrase})]".format(token=token, tokenized_phrase=tokenized_phrase)
+def _custom_token_handler(token: str, phrase: str) -> str:
+    return '[{token}({phrase})]'.format(token=token, phrase=phrase)
 
 def main() -> None:
-    input_path = Path(__file__).parent / "patterns.yml"
-    dynamic_token_patterns_definition = {
-        "ACTION": ((("tell me the news", "what's the weather"),),)
+    pattern_definition_path = Path(__file__).parent / 'patterns.yml'
+    dynamic_token_to_token_patterns = {
+        'SONG': ((('here comes the sun', 'stronger'),),)
     }
     token_handlers = {
-        "WAKE": _custom_token_handler,
-        "SONG": _custom_token_handler,
-        "ACTION": _custom_token_handler
+        'WAKE': _custom_token_handler,
+        'SONG': _custom_token_handler,
     }
-    for utterance_pattern, tokens in generate_utterance_pattern_and_tokens(input_path,
-                                                                           dynamic_token_patterns_definition):
-        # set options specific to each utterance_pattern
+    for utt_pattern, utt_pattern_tokens in generate_utterance_pattern_and_tokens(pattern_definition_path,
+                                                                                 dynamic_token_to_token_patterns):
+        # here you could set combination options specific to each utterance_pattern
         combination_options = CombinationOptions(max_sample_size=5, with_replacement=False, seed=0)
-        for utterance, token_str in generate_utterances_and_tokens(utterance_pattern,
-                                                                   tokens,
-                                                                   token_handlers,
-                                                                   combination_options):
-            print("utterance: ", utterance)
-            print("tokens: ", token_str)
-        # cap iteration at X to speed it up
+        for utterance, tokens in generate_utterances_and_tokens(utt_pattern,
+                                                                utt_pattern_tokens,
+                                                                token_handlers,
+                                                                combination_options):
+            print('utterance: ', utterance)
+            print('tokens: ', tokens)
 
 
 if __name__ == '__main__':
